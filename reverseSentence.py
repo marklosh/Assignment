1e07d21c60f532b3c674ab8881317ae0f7a768e5
sabu <markloshilu@gmail.com>
2021-05-21 11:23:59 +0300

    added a reverseSentence.py file that takes in an array of characters, reverses the the individual words  and returns the reversed array

---
+++ reverseSentence.py
@@ -0,0 +1,10 @@
+def reverseSentence(A):
+    """
+    the above function takes in an array of characters, A, and reverses the the "words"
+    (not individual characters).
+    Example:
+
+    A = ['t','h','i','s',' ','i','s',' ','g','o','o','d'] and
+    returns ['g','o','o','d',' ','i','s',' ','t','h','i','s']
+    """
+    ###still working on the slution
